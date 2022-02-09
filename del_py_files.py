d1bf2f459dc0513d2c517344158133b66911def7
aengusng <aengus.ng8@gmail.com>
2022-02-09 23:32:57 +0700

    deleted all .py files

---
+++ del_py_files.py
@@ -0,0 +1,20 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--folder', type=str)
+
+def del_py_files(FOLDER_PATH):
+    lst = os.listdir(f"{FOLDER_PATH}/")
+    print(FOLDER_PATH)
+
+    for obj in lst:
+        print(obj)
+        if os.path.isfile(f"{FOLDER_PATH}/{obj}"):
+            if obj[-3:] == ".py" and obj != "del_py_files.py":
+                os.remove(f"{FOLDER_PATH}/{obj}")
+        else:
+            del_py_files(f"{FOLDER_PATH}/{obj}")
+
+args = parser.parse_args()
+del_py_files(args.folder)
